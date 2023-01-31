fba76f303300e373a99beccf364ec57423fa12e9
zora89 <zorawar.purohit@gmail.com>
2023-02-01 02:57:02 +0530

    exlixir :atom, string, UTF-8

--- simple.exs
+++ simple.exs
@@ -1,33 +1,122 @@
-#print your first hello world
-IO.puts("Hello world from Elixir")
+# One Line Comments Are Written Along Side Code (using->)  "#"
+# Another comment, got it !!
+# Lookout for > #input (you can play with that line, as per "<hints>")
+# Let us dive right in ....
 
+# Data Types in Elixir, and most programming languages are the same.
 
-# Data Types in `elixir`
-
-:atom
-_ = "elixir"
+# 1          # integer
+# 0x1F       # integer
+# 1.0        # float
+# true       # boolean
+# :atom      # atom / symbol
+# "elixir"   # string
+# [1, 2, 3]  # list
+# {1, 2, 3}  # tuple
 
 # Basic arithmetic is easy, we use it daily. Even while buying vegetables, so go for it !
 
-assign_varialble_a = 1+2
+assign_variable_a = 1+2
 assign_variable_b = 5*5
-assign_varialble_c = 10/2
+assign_variable_c = 10/2
 divide = div(10,2)
 remainder = rem(10,2)
-IO.puts(assign_varialble_a)
+
+IO.puts("\vResults for Variable Assignments") #\v is vertical tab \n is new line
+IO.puts(assign_variable_a)
 IO.puts(assign_variable_b)
-IO.puts(assign_varialble_c)
+IO.puts(assign_variable_c)
 IO.puts(divide)
 IO.puts(remainder)
 
-#One Line Comments -> Using "#" -> IO.puts(assign_varialble_c)
-#Another Comment, Got it !
-
 # Shortcut for Binary, Octal & Hexadecimal numbers
+
 binary = 0b1010
 octal = 0o777
 hexadecimal = 0x1F
+float_numbers = 1.0
+scientific_numbers = 1.0e-10
+
+IO.puts("\vResults for Shortcut Binary, Octal, Hexadecimal, Float & Scientific Numbers")
 IO.puts(binary)
 IO.puts(octal)
 IO.puts(hexadecimal)
 IO.puts(0b1010) #direct binary output without variable assignment
+IO.puts(float_numbers)
+IO.puts(scientific_numbers)
+
+# Help commands for terminal -> input without " "
+
+launch = "iex"
+documentation = "h"
+documentation_example = "h Kernel.+/2" # show Kernel based '+' operators by arity (fixed number of arguments) 2
+
+# Boolean true or false (note: smallcase)
+
+truish = true
+falsy = false
+
+IO.puts("\vResults Boolean")
+IO.puts(truish)
+IO.puts(falsy)
+
+# In-Built Data Type Check Functions with arity /1
+
+IO.puts("\vResults for Data Type Checks")
+IO.puts(is_boolean(truish))
+IO.puts(is_integer(assign_variable_a))
+IO.puts(is_float(assign_variable_b))
+IO.puts(is_number(assign_variable_c))
+
+# Atoms (:atom) are the key, pay attention !!
+
+:bhindi
+:lauki
+:karela
+
+# Equality check among :atom
+
+IO.puts("\vResults from Equality Check :atoms")
+
+IO.puts(:bhindi == :bhindi)
+IO.puts(:lauki == :bhindi)
+:bhindi == :lauki
+IO.puts(:lauki == :bhindi)
+IO.puts(:bhindi)
+
+IO.puts("\vResults from first :atom assignment")
+
+format_string = :wow # first assignment of an :atom
+
+"#{}" # formating leads with #{}
+IO.puts("Hello #{format_string}")
+
+# String Length
+
+IO.puts(
+  "\vResult from grapheme byte example, how strings are stored internally as binaries"
+  )
+IO.puts(is_binary("Hello World")) #representation of strings (graphemes) represented internally as bytes -> binaries
+IO.puts("\vResult from byte_size")
+IO.puts(byte_size(("Hello")))
+IO.puts("\vResult from String.length")
+IO.puts(String.length(("Hello World")))
+
+# Concatenate String and Interpolation
+
+IO.puts("\vResults from Concatenation and Interpolation")
+
+z = "<Your-Name-Here>" # Input
+p = "<Your-Name-Here>" # Input
+
+concatenate = "#{z} " <> "#{p}"
+IO.puts(concatenate)
+
+IO.puts("\vResults from in-string interpolation of math, variable, :atom and \\ use")
+
+IO.puts("\v2 + 2 \" #{2 + 2} \" \# \\\\ ") # Note use of backslash \ for " # \ \\ use in string
+
+IO.puts(?A)
+IO.puts(Integer.to_string(?A, 16))
+test_string_utf_hex = 0x0041 = 65 = ?A
+IO.puts("\vString to UTF to Hex #{test_string_utf_hex}")
