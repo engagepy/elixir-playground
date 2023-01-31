a060d67b8b43d7983c5dd1eb17cb7e41173f83fa
zora89 <zorawar.purohit@gmail.com>
2023-02-01 00:42:11 +0530

    elixir arithmetics

--- simple.exs
+++ simple.exs
@@ -2,7 +2,32 @@
 IO.puts("Hello world from Elixir")
 
 
-#data types
+# Data Types in `elixir`
 
 :atom
-"elixir"
+_ = "elixir"
+
+# Basic arithmetic is easy, we use it daily. Even while buying vegetables, so go for it !
+
+assign_varialble_a = 1+2
+assign_variable_b = 5*5
+assign_varialble_c = 10/2
+divide = div(10,2)
+remainder = rem(10,2)
+IO.puts(assign_varialble_a)
+IO.puts(assign_variable_b)
+IO.puts(assign_varialble_c)
+IO.puts(divide)
+IO.puts(remainder)
+
+#One Line Comments -> Using "#" -> IO.puts(assign_varialble_c)
+#Another Comment, Got it !
+
+# Shortcut for Binary, Octal & Hexadecimal numbers
+binary = 0b1010
+octal = 0o777
+hexadecimal = 0x1F
+IO.puts(binary)
+IO.puts(octal)
+IO.puts(hexadecimal)
+IO.puts(0b1010) #direct binary output without variable assignment
